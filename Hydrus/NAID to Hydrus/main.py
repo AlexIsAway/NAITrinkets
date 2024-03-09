cad5281e630c4d38f57a67b68bc4c10d93760216
AlexIsAway <153025506+AlexIsAway@users.noreply.github.com>
2024-03-08 21:06:26 -0500

    Update NAID->Hydrus
    Colored text, some other lame stuff

--- Hydrus/NAID to Hydrus/main.py
+++ Hydrus/NAID to Hydrus/main.py
@@ -4,7 +4,71 @@ import exiftool
 import hydrus_api
 import hydrus_api.utils
 import requests
-regularImport = ""
+hasColored = False
+Style = None
+Fore = None
+Back = None
+try:
+    from colored import Style, Fore, Back, set_tty_aware
+    hasColored = True
+except ImportError:
+    print('No Colored Package')
+###
+# yoinked from stackoverflow
+###
+from sys import stdin, stdout
+from platform import system
+if system() == "Windows":
+    from msvcrt import getch, kbhit
+else:
+    from termios import TCSADRAIN, tcgetattr, tcsetattr
+    from select import select
+    from tty import setraw
+    from sys import stdin
+    def getch() -> bytes:
+        fd = stdin.fileno()
+        old_settings = tcgetattr(fd)
+        try:
+            setraw(fd)
+            return stdin.read(1).encode()
+        finally:
+            tcsetattr(fd, TCSADRAIN, old_settings)
+    def kbhit() -> bool:
+        return bool(select([stdin], [], [], 0)[0])
+def isansitty() -> bool:
+    while kbhit():
+        getch()
+    stdout.write("\x1b[6n")
+    stdout.flush()
+    stdin.flush()
+    if kbhit():
+        if ord(getch()) == 27 and kbhit():
+            if getch() == b"[":
+                while kbhit():
+                    getch()
+
+                return stdout.isatty()
+    stdout.write('\b\b\b\b')
+    return False
+if not isansitty():
+    hasColored = False
+if not hasColored:
+    # Mimic colored package with empty values
+    class Style:
+        reset = ''
+        underline = ''
+    class Fore:
+        red = ''
+        green = ''
+        blue = ''
+        magenta = ''
+        light_yellow = ''
+    class Back:
+        red = ''
+        dark_red_1 = ''
+        black = ''
+    # only mimic used values
+###
 importingFiles = []
 currentTagList = []
 importPath = ""
@@ -33,15 +97,20 @@ def mainMenu(inp = None):
     if confdata != "":
         configData = json.loads(confdata)
     if configData.get('latestCommit') != None and latestCommit['sha'] != configData['latestCommit'] and latestCommit['sha'] != None and latestCommit['sha'] != "" and latestCommit['commit']['message'].find('NAID->Hydrus') != -1:
-        print('New Update Available! -> '+latestCommit['commit']['description'])
-        print('Download at:\nhttps://github.com/AlexIsAway/NAITrinkets/tree/main/Hydrus/NAID%20to%20Hydrus\n\n')
+        nud = latestCommit['commit']
+        if nud.get('description') != None:
+            nud = nud['description']
+        else:
+            nud = nud['message']
+        print(f'\n{Fore.red}New Update Available!{Style.reset} -> {nud}')
+        print(f'\nDownload at:\n{Fore.blue}{Style.underline}https://github.com/AlexIsAway/NAITrinkets/tree/main/Hydrus/NAID%20to%20Hydrus{Style.reset}')
     if inp == None:
         global regularImport
-        regularImport = input("\n>| NAIDiffusion -> Hydrus |<\n\n0 - Make Sidecar Files\n1 - Import Directly via API\n2 - Change Config\n\nA: ").lower()
+        regularImport = input(f"\n{Fore.blue}>| NAIDiffusion -> Hydrus |<{Style.reset}\n\n0 - Make Sidecar Files\n1 - Import Directly via API\n2 - Change Config\n\nA: ").lower()
     config.seek(0)
     if config.readable() and confdata != "" and regularImport != "2":
-        print('Config Found')
-        print(configData)
+        print(f'{Fore.green}Config Found{Style.reset}')
+        #print(configData)
         importPath = configData["importPath"]
         client = hydrus_api.Client(configData["accessKey"])
         servicekey = configData["serviceKey"]
@@ -49,7 +118,7 @@ def mainMenu(inp = None):
     elif regularImport == "2":
         importPath = input("Enter the path to the directory you want to import (entire absolute path):\n")
         if os.path.exists(importPath) == False:
-            print('Invalid Path')
+            print(f'{Fore.red}Invalid Path{Style.reset}')
             mainMenu('')
             return
         client = hydrus_api.Client(input("Enter your access key:\n"))
@@ -58,24 +127,24 @@ def mainMenu(inp = None):
         mainMenu()
         return
     elif (regularImport == "1") and confdata == "":
-        print('No Config Found')
+        print(f'{Fore.yellow}No Config Found{Style.reset}')
         importPath = input("Enter the path to the directory you want to import (entire absolute path):\n")
         if os.path.exists(importPath) == False:
-            print('Invalid Path')
+            print(f'{Fore.red}Invalid Path{Style.reset}')
             mainMenu('')
             return
         client = hydrus_api.Client(input("Enter your access key:\n"))
         servicekey = input("Enter a file domain service key:\n")
         config.write(json.dumps({"importPath": importPath, "accessKey": client.access_key, "serviceKey": servicekey, "latestCommit": latestCommit['sha']})) 
     elif regularImport == "0" and confdata == "":
-        print('No Config Found')
+        print(f'{Fore.yellow}No Config Found{Style.reset}')
         importPath = input("Enter the path to the directory you want to import (entire absolute path):\n")
         if os.path.exists(importPath) == False:
-            print('Invalid Path')
+            print(f'{Fore.red}Invalid Path{Style.reset}')
             mainMenu('')
             return
         config.write(json.dumps({"importPath": importPath, "accessKey": "", "serviceKey": "", "latestCommit": latestCommit['sha']}))
-    print(importPath,client.access_key,servicekey)
+    #print(importPath,client.access_key,servicekey)
 mainMenu()
 if importPath == "":
     mainMenu(True)
@@ -137,30 +206,30 @@ def importFiles(files):
                     print('Last File')
                     res = hydrus_api.utils.add_and_tag_files(client, importingFiles, currentTagList, ['6c6f63616c2074616773'])
                     if res and res[0] and res[0]['status'] and (res[0]['status'] == 2 or res[0]['status'] == 1):
-                        print('Successful Import')
+                        print(f'{Fore.green}Successful Import{Style.reset}')
                         for filex in importingFiles:
                             os.remove(f"{filex}")
                             print('Deleted Imported File -> '+filex)
                     else:
-                        print('Failed Import!')
+                        print(f'{Fore.red}Failed Import!{Style.reset}')
         except:
-            print('Failed to import '+file)
+            print(f'{Fore.red}Failed to import{Style.reset} '+file)
             continue
     if sortList != []:
         importFiles(sortList)
 config.close()
 if regularImport == "0":
-    if input('!WARNING!\nThis will create a sidecar file for every image in the directory, and insert it into the directory so that you can import it manually into Hydrus. (recommended for large imports) Are you sure you want to continue?\n(y/n)\n').lower() != "y":
+    if input(f'{Fore.light_yellow}!WARNING!\n{Back.red}This will create a sidecar file for every image in the directory, and insert it into the directory so that you can import it manually into Hydrus.\n{Style.reset}Are you sure you want to continue? (y/n)\n').lower() != "y":
         print('Exiting...')
         exit()
 elif regularImport == "1":
-    if input('!WARNING!\nThis will import every image in the directory and delete the file afterwards to avoid clutter (recommended for one time imports). Are you sure you want to continue?\n(y/n)\n').lower() != "y":
+    if input(f'{Back.dark_red_1}{Fore.light_yellow}!WARNING!{Back.black}\n{Fore.red}This will import every image in the directory and delete the file afterwards!\n{Style.reset}Are you sure you want to continue? (y/n)\n').lower() != "y":
         print('Exiting...')
         exit()
 elif regularImport == "2":
-    print('Config Saved')
+    print(f'{Fore.green}Config Saved{Style.reset}')
     exit()
 importFiles(allFiles)
         
-print('Done!')
+print(f'{Fore.magenta}Done!{Style.reset}')
 exit()
