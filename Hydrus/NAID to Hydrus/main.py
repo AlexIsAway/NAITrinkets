94ea5603facc49ccd00608bad499742ac7ab1960
AlexIsAway <153025506+AlexIsAway@users.noreply.github.com>
2024-03-08 20:00:52 -0500

    Update NAID->Hydrus
    Better filtering for auto-upload, removed most of the prints, tells you when the file has updated and what's been updated.

--- Hydrus/NAID to Hydrus/main.py
+++ Hydrus/NAID to Hydrus/main.py
@@ -3,6 +3,7 @@ import json
 import exiftool
 import hydrus_api
 import hydrus_api.utils
+import requests
 regularImport = ""
 importingFiles = []
 currentTagList = []
@@ -10,6 +11,9 @@ importPath = ""
 client = None;
 servicekey = ""
 config = None
+latestCommit = requests.get('https://api.github.com/repos/AlexIsAway/NAITrinkets/commits/main').json()
+# print('Latest Commit: '+latestCommit['sha'])
+# print('Commit Name: '+latestCommit['commit']['message'])
 try:
     config = open("config.json", "r+")
 except:
@@ -22,17 +26,26 @@ def mainMenu(inp = None):
     global importPath
     global client
     global servicekey
+    confdata = ""
+    configData:dict = {}
+    if config.readable():
+        confdata = config.read()
+    if confdata != "":
+        configData = json.loads(confdata)
+    if configData.get('latestCommit') != None and latestCommit['sha'] != configData['latestCommit'] and latestCommit['sha'] != None and latestCommit['sha'] != "" and latestCommit['commit']['message'].find('NAID->Hydrus') != -1:
+        print('New Update Available! -> '+latestCommit['commit']['description'])
+        print('Download at:\nhttps://github.com/AlexIsAway/NAITrinkets/tree/main/Hydrus/NAID%20to%20Hydrus\n\n')
     if inp == None:
         global regularImport
         regularImport = input("\n>| NAIDiffusion -> Hydrus |<\n\n0 - Make Sidecar Files\n1 - Import Directly via API\n2 - Change Config\n\nA: ").lower()
-    confdata = config.read()
+    config.seek(0)
     if config.readable() and confdata != "" and regularImport != "2":
         print('Config Found')
-        configData = json.loads(confdata)
         print(configData)
         importPath = configData["importPath"]
         client = hydrus_api.Client(configData["accessKey"])
         servicekey = configData["serviceKey"]
+        config.write(json.dumps({"importPath": importPath, "accessKey": client.access_key, "serviceKey": servicekey, "latestCommit": latestCommit['sha']}))
     elif regularImport == "2":
         importPath = input("Enter the path to the directory you want to import (entire absolute path):\n")
         if os.path.exists(importPath) == False:
@@ -41,7 +54,7 @@ def mainMenu(inp = None):
             return
         client = hydrus_api.Client(input("Enter your access key:\n"))
         servicekey = input("Enter a file domain service key:\n")
-        config.write(json.dumps({"importPath": importPath, "accessKey": client.access_key, "serviceKey": servicekey}))
+        config.write(json.dumps({"importPath": importPath, "accessKey": client.access_key, "serviceKey": servicekey, "latestCommit": latestCommit['sha']}))
         mainMenu()
         return
     elif (regularImport == "1") and confdata == "":
@@ -53,7 +66,7 @@ def mainMenu(inp = None):
             return
         client = hydrus_api.Client(input("Enter your access key:\n"))
         servicekey = input("Enter a file domain service key:\n")
-        config.write(json.dumps({"importPath": importPath, "accessKey": client.access_key, "serviceKey": servicekey})) 
+        config.write(json.dumps({"importPath": importPath, "accessKey": client.access_key, "serviceKey": servicekey, "latestCommit": latestCommit['sha']})) 
     elif regularImport == "0" and confdata == "":
         print('No Config Found')
         importPath = input("Enter the path to the directory you want to import (entire absolute path):\n")
@@ -61,27 +74,35 @@ def mainMenu(inp = None):
             print('Invalid Path')
             mainMenu('')
             return
-        config.write(json.dumps({"importPath": importPath, "accessKey": "", "serviceKey": ""}))
+        config.write(json.dumps({"importPath": importPath, "accessKey": "", "serviceKey": "", "latestCommit": latestCommit['sha']}))
     print(importPath,client.access_key,servicekey)
 mainMenu()
 if importPath == "":
     mainMenu(True)
 allFiles = os.listdir(importPath)
 et = exiftool.ExifToolHelper()
-def importFiles():
-    for file in allFiles:
+# def sortFunc(str):
+#     return str.lower()
+#allFiles.sort(key=sortFunc)
+def importFiles(files):
+    #print(files)
+    sortList = []
+    global currentTagList
+    global importingFiles
+    currentTagList = []
+    importingFiles = []
+    for file in files:
         try:
             if file.endswith(".png"):
                 #i hate python (and learning it)
-                global currentTagList
-                global importingFiles
                 global regularImport
                 global importPath
                 global client
                 global servicekey
                 finalTagList = []
+                #print(f"{importPath}\\{file}")
                 metadata = et.get_metadata(f"{importPath}\\{file}")
-                print('Exporting '+file)
+                #print('Exporting '+file)
                 if metadata[0] != None and metadata[0].get('PNG:Comment') != None:
                     mainData:dict = json.loads(metadata[0]['PNG:Comment'])
                     for key in mainData:
@@ -101,36 +122,32 @@ def importFiles():
                 lookingfor = ""
                 for tagFound in comparingTagList:
                     if tagFound.find("seed:") != -1:
-                        print('Found Tag -> '+tagFound)
+                        #print('Found Tag -> '+tagFound)
                         lookingfor = tagFound
                 if lookingfor != "":
                     comparingTagList.remove(lookingfor)
+                if currentTagList == [] and comparingTagList != []:
+                    currentTagList = comparingTagList
                 if currentTagList != comparingTagList and currentTagList != []:
-                    print('Tag List Changed')
-                    res = hydrus_api.utils.add_and_tag_files(client, importingFiles, currentTagList, ['6c6f63616c2074616773'])
-                    if res and res[0] and res[0]['status'] and (res[0]['status'] == 2 or res[0]['status'] == 1):
-                        print('Successful Import')
-                        for filex in importingFiles:
-                            os.remove(f"{filex}")
-                            print('Deleted Imported File -> '+file)
-                    else:
-                        print('Failed Import!')
-                    importingFiles = []
-                currentTagList = comparingTagList
-                importingFiles.append(f"{importPath}\\{file}")
-                if allFiles.index(file) == len(allFiles)-1:
+                    #print('Tag List Changed\nCurrent:\n\n'+currentTagList.__str__()+"\n\nDifference:\n\n"+comparingTagList.__str__())
+                    sortList.append(f"{file}")
+                elif currentTagList == comparingTagList:
+                    importingFiles.append(f"{importPath}\\{file}")
+                if files.index(file) == len(files)-1 and importingFiles != [] and currentTagList != []:
                     print('Last File')
                     res = hydrus_api.utils.add_and_tag_files(client, importingFiles, currentTagList, ['6c6f63616c2074616773'])
                     if res and res[0] and res[0]['status'] and (res[0]['status'] == 2 or res[0]['status'] == 1):
                         print('Successful Import')
                         for filex in importingFiles:
                             os.remove(f"{filex}")
-                            print('Deleted Imported File -> '+file)
+                            print('Deleted Imported File -> '+filex)
                     else:
                         print('Failed Import!')
         except:
             print('Failed to import '+file)
             continue
+    if sortList != []:
+        importFiles(sortList)
 config.close()
 if regularImport == "0":
     if input('!WARNING!\nThis will create a sidecar file for every image in the directory, and insert it into the directory so that you can import it manually into Hydrus. (recommended for large imports) Are you sure you want to continue?\n(y/n)\n').lower() != "y":
@@ -143,7 +160,7 @@ elif regularImport == "1":
 elif regularImport == "2":
     print('Config Saved')
     exit()
-importFiles()
+importFiles(allFiles)
         
 print('Done!')
 exit()
